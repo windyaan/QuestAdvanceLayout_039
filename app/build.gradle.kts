97f4a647eaf00100dae11270f6358a44b4c56c18
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-08-05 20:52:48 +0700

    edit gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.questadvancelayout_039"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.questadvancelayout_039"
